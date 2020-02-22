3601da6c4d6b0294fff36d1efe887fe1b2dffe62
Dennis Kwabena Bilson Jr <41457025+Quabynah-Codelabs@users.noreply.github.com>
2020-02-22 18:40:14 +0000

    Added actions

--- 2020-apps/project-app-folder/app/src/main/java/shortcourse/legere/MainActivity.kt
+++ 2020-apps/project-app-folder/app/src/main/java/shortcourse/legere/MainActivity.kt
@@ -1,19 +1,31 @@
 package shortcourse.legere
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
-import com.google.android.material.button.MaterialButton
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import kotlinx.android.synthetic.main.activity_main.*
+import shortcourse.legere.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
         // Old way
         // findViewById<MaterialButton>(R.id.get_started)
 
-        get_started.setOnClickListener {  }
+        // Another way
+        // get_started.setOnClickListener { }
+
+        binding.getStarted.setOnClickListener {
+            // Intent
+            val intent = Intent(this,HomeActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
 
     }
 }
