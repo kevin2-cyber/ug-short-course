b4e1bf3e7e01eecb2771fbb0ee229f23c16a9789
Dennis Kwabena Bilson Jr <41457025+Quabynah-Codelabs@users.noreply.github.com>
2020-02-22 18:24:36 +0000

    Types & colors

--- 2020-apps/project-app-folder/app/src/main/java/shortcourse/legere/MainActivity.kt
+++ 2020-apps/project-app-folder/app/src/main/java/shortcourse/legere/MainActivity.kt
@@ -2,10 +2,18 @@ package shortcourse.legere
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.button.MaterialButton
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Old way
+        // findViewById<MaterialButton>(R.id.get_started)
+
+        get_started.setOnClickListener {  }
+
     }
 }
