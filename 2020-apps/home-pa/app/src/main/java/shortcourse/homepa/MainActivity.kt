9cd4ac13e99c4061427d878d97803572c763815a
Dennis Kwabena Bilson Jr <41457025+Quabynah-Codelabs@users.noreply.github.com>
2020-02-29 17:46:02 +0000

    Added Firebase

--- 2020-apps/home-pa/app/src/main/java/shortcourse/homepa/MainActivity.kt
+++ 2020-apps/home-pa/app/src/main/java/shortcourse/homepa/MainActivity.kt
@@ -2,10 +2,14 @@ package shortcourse.homepa
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.FirebaseApp
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Initialize Firebase
+        FirebaseApp.initializeApp(this)
     }
 }
