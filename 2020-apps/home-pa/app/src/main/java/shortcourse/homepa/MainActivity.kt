345bccfe9ee183d116c0f938fdf00034eaa93a65
Dennis Kwabena Bilson Jr <41457025+Quabynah-Codelabs@users.noreply.github.com>
2020-02-29 18:13:04 +0000

    Added DI for Firebase SDKs

--- 2020-apps/home-pa/app/src/main/java/shortcourse/homepa/MainActivity.kt
+++ 2020-apps/home-pa/app/src/main/java/shortcourse/homepa/MainActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.firestore.FirebaseFirestore
 import org.koin.android.ext.android.inject
-import shortcourse.homepa.util.debugger
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,18 +11,12 @@ class MainActivity : AppCompatActivity() {
     private val firestore by inject<FirebaseFirestore>()
 
     // Without DI
-    // private val firestore1 by lazy { FirebaseFirestore.getInstance() }
+    // private val firestore by lazy { FirebaseFirestore.getInstance() }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        debugger(firestore)
 
-        firestore.collection("demo")
-            .get()
-            .addOnCompleteListener(this) { task ->
-                debugger(task.isSuccessful)
-            }
     }
 }
--- 2020-apps/home-pa/app/src/main/java/shortcourse/homepa/injection/KoinModules.kt
+++ 2020-apps/home-pa/app/src/main/java/shortcourse/homepa/injection/KoinModules.kt
@@ -1,9 +1,14 @@
 package shortcourse.homepa.injection
 
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.storage.FirebaseStorage
 import org.koin.dsl.module
 
 // This contains all Firebase SDKs used in the application
 val firebaseModule = module {
     single { FirebaseFirestore.getInstance() }
+    single { FirebaseAuth.getInstance() }
+    single { FirebaseStorage.getInstance().reference.child("home-pa") }
+
 }
